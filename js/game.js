8ba193f7d54c99763a32e1f8eeed357c407f88ba
Anastasiia Yermakova <yermak1037@gmail.com>
2020-08-14 21:53:44 +0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -7,6 +7,9 @@ ground.src = './img/ground.png';
 const foodImg = new Image();
 foodImg.src = './img/food.png';
 
+const snakeImg = new Image();
+snakeImg.src = './img/snake.png';
+
 let box = 32;
 
 let score = 0;
@@ -32,6 +35,30 @@ function direction(event) {
   else if (event.keyCode == 39 && dir != 'left') dir = 'right';
   else if (event.keyCode == 40 && dir != 'up') dir = 'down';
 }
+
+function sound(src) {
+  this.sound = document.createElement('audio');
+  this.sound.src = src;
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document.body.appendChild(this.sound);
+  this.play = function () {
+    this.sound.play();
+  };
+  this.stop = function () {
+    this.sound.pause();
+  };
+}
+
+const eatSound = new sound('./audio/EatSound.ogg');
+const dieSound = new sound('./audio/DieSound.ogg');
+
+const forceReload = () => {
+  clearInterval(game);
+  location.reload(true);
+  dieSound.stop();
+};
 
 function eatTail(head, arr) {
   for (let i = 0; i < arr.length; i += 1) {
@@ -61,6 +88,7 @@ function drawGame() {
   let snakeY = snake[0].y;
 
   if (snakeX == food.x && snakeY == food.y) {
+    eatSound.play();
     score += 1;
     food = {
       x: Math.floor(Math.random() * 17 + 1) * box,
@@ -76,6 +104,7 @@ function drawGame() {
     snakeY < 3 * box ||
     snakeY > box * 17
   ) {
+    dieSound.play();
     clearInterval(game);
     alert('Game Over! Score: ' + score);
     window.location.reload();
